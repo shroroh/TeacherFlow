8eafab318964f5396831ac5b7de2ae3046df16bf
Dmutrii <vitrage091@gmail.com>
2026-03-01 02:32:31 +0300

    Remove entry point usage instructions
    Removed usage instructions from the main.py file.

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-"""Entry point for TeacherFlow - generate personalized teacher feedback.
-
-Usage:
-    python main.py --student-id ivan123
-    python -m teacherflow --student-id ivan123
-"""
-
 from teacherflow._cli import main
 
 
